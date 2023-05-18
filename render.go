072241a740f6f4e680452e1b3fff177b854b4d3b
chris <christhirst@gmail.com>
2023-05-18 22:13:35 +0200

    hopefully fixxed render

--- render.go
+++ render.go
@@ -3,7 +3,6 @@ package oauth
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/rs/zerolog/log"
@@ -11,24 +10,20 @@ import (
 
 // renderJSON marshals 'v' to JSON, automatically escaping HTML, setting the
 // Content-Type as application/json, and sending the status code header.
-func renderJSON(w http.ResponseWriter, v interface{}, statusCode int) error {
+func renderJSON(w http.ResponseWriter, v interface{}, statusCode int) {
 	buf := &bytes.Buffer{}
 	enc := json.NewEncoder(buf)
 	enc.SetEscapeHTML(true)
-
 	b, err := json.MarshalIndent(v, "", "\t")
 	if err != nil {
 		log.Error().Err(err).Msg("failed to marshal object to json")
 	}
-	defer buf.Reset()
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(statusCode)
 
-	if _, err := w.Write(b); err != nil {
-		return fmt.Errorf("failed to write response body: %v", err)
+	_, err = w.Write(b)
+	if err != nil {
+		log.Error().Err(err).Msg("render json failed")
 	}
-	buf.Reset()
-	return nil
-
 }
